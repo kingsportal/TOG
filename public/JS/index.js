3143d8c36e9e2acf58f9e4126a4824ff9f96f088
koushikamboru <koushic1980@gmail.com>
2024-03-08 01:35:46 +0530

    css

---
+++ public/JS/index.js
@@ -0,0 +1,10 @@
+/*Start of Navgation active status*/
+function setActive(element) 
+{
+    const navItems = document.querySelectorAll('.nav_pages li');
+    navItems.forEach(item => {
+      item.classList.remove('active');
+    });
+    element.parentElement.classList.add('active');
+}
+/*ENd of Navgation active status*/
